8ec16a81cd50e579a5dbe2564892671defdd467c
Szymok <szymonkowal019@gmail.com>
2024-01-08 12:46:36 +0100

    add generating images

--- app.py
+++ app.py
@@ -73,3 +73,33 @@ def generate_tweets(topic: str, mood: str = '', style: str = ''):
                     f'Tweet: {st.session_state.tweet}'
                 )
 
+def generate_image(prompt: str):
+    '''Generate Tweet Image'''
+    if st.session_state.n_requests >= 5:
+        st.session_state.text_error = 'Too many requests. Please try again later.'
+        logging.info(f'Session request limit reached: {st.session_state.n_requests}')
+        st.session_state.n_requests = 1
+        return
+    
+    with image_spinner_placeholder:
+        with st.spinner('Please wait while your image is being generated...'):
+            openai = oai.Openai()
+            prompt_wo_hashtags = re.sub('#[A-Za-z0-9]+', '', prompt)
+            processing_prompt = (
+                'Create a detailed but brief description of an image that captures '
+                f'the essence of the following text:\n{prompt_wo_hashtags}\n\n'
+            )
+            processed_prompt = (
+                openai.complete(
+                    prompt=processing_prompt, temperature=0.5, max_tokens=40
+                )
+                .strip()
+                .replace('"', "")
+                .split('.')[0]
+                + '.'
+            )
+            st.session_state.n_requests += 1
+            st.session_state.image = openai.image(processed_prompt)
+            logging.info(f'Tweet: {prompt}\nImage prompt: {processed_prompt}')
+
+
